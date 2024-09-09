2b9aae1ed277352233541d0e59e9097d9d092a5b
Tsahi Duek <tsahiduek@gmail.com>
2024-09-09 17:59:49 +0100

    new folder for AWS Native observability

---
+++ single-account-single-cluster-multi-env/40.observability/40.aws-native-observability/main.tf
@@ -0,0 +1,50 @@
+data "aws_region" "current" {}
+
+data "aws_caller_identity" "current" {}
+
+locals {
+  region = data.aws_region.current.id
+}
+################################################################################
+# CW EKS Addon
+################################################################################
+module "aws_cloudwatch_observability_irsa" {
+
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5.44"
+  count   = var.observability_configuration.aws_native_tooling ? 1 : 0
+
+  role_name = "${data.terraform_remote_state.eks.outputs.cluster_name}-cw-ci"
+
+  role_policy_arns = {
+    policy = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+  }
+
+  oidc_providers = {
+    cluster = {
+      provider_arn               = data.terraform_remote_state.eks.outputs.oidc_provider_arn
+      namespace_service_accounts = ["amazon-cloudwatch:cloudwatch-agent"]
+    }
+  }
+}
+
+module "aws_cloudwatch_observability" {
+  source  = "aws-ia/eks-blueprints-addons/aws"
+  version = "~> 1.16.2"
+  count   = var.observability_configuration.aws_native_tooling ? 1 : 0
+
+  cluster_name      = data.terraform_remote_state.eks.outputs.cluster_name
+  cluster_endpoint  = data.terraform_remote_state.eks.outputs.cluster_endpoint
+  cluster_version   = data.terraform_remote_state.eks.outputs.kubernetes_version
+  oidc_provider_arn = data.terraform_remote_state.eks.outputs.oidc_provider_arn
+
+  create_kubernetes_resources = true
+  eks_addons = {
+    amazon-cloudwatch-observability = {
+      most_recent              = true
+      service_account_role_arn = module.aws_cloudwatch_observability_irsa.iam_role_arn
+    }
+  }
+}
+
+
---
+++ single-account-single-cluster-multi-env/40.observability/40.aws-native-observability/providers.tf
@@ -0,0 +1,76 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.40.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.22.0"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.7"
+    }
+    kubectl = {
+      source  = "alekc/kubectl"
+      version = ">= 2.0.3"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.0"
+    }
+  }
+
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.terraform_remote_state.eks.outputs.cluster_endpoint
+    cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.cluster_certificate_authority_data)
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      # This requires the awscli to be installed locally where Terraform is executed
+      args = ["eks", "get-token", "--cluster-name", data.terraform_remote_state.eks.outputs.cluster_name, "--region", local.region]
+
+    }
+  }
+}
+
+provider "kubernetes" {
+  host                   = data.terraform_remote_state.eks.outputs.cluster_endpoint
+  cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    # This requires the awscli to be installed locally where Terraform is executed
+    args = ["eks", "get-token", "--cluster-name", data.terraform_remote_state.eks.outputs.cluster_name, "--region", local.region]
+  }
+}
+
+provider "aws" {
+  default_tags {
+    tags = local.tags
+  }
+}
+
+provider "kubectl" {
+  apply_retry_count      = 5
+  host                   = data.terraform_remote_state.eks.outputs.cluster_endpoint
+  cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.cluster_certificate_authority_data)
+  load_config_file       = false
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    # This requires the awscli to be installed locally where Terraform is executed
+    args = ["eks", "get-token", "--cluster-name", data.terraform_remote_state.eks.outputs.cluster_name, "--region", local.region]
+
+  }
+}
